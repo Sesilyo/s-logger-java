005739b78eb0dfe83beb89b72300ee3a5507815a
Sesilyo <c.sethleander@gmail.com>
2026-08-17 02:51:48 +0800

    chore: initial MVC structure and partial console constants

--- src/main/java/com/seth/slogger/Main.java
+++ src/main/java/com/seth/slogger/Main.java
@@ -1,8 +1,14 @@
+// FILENAME: Main.java
 package com.seth.slogger;
+
+// imports
+import com.seth.slogger.view.ConsoleView;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("S-LOGGER v1.0");
+		boolean running = true;
+		while (running) {
+			System.out.print(ConsoleView.MAIN_MENU);
+		}
 	}
 }
